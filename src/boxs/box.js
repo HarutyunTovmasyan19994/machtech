2297043e542ba8681a35385706bfd08fa7c1e144
Harutyun <91012818+HarutyunTovmasyan19994@users.noreply.github.com>
2024-01-20 23:49:48 +0400

    boxs size

--- src/boxs/box.js
+++ src/boxs/box.js
@@ -10,14 +10,14 @@ const Boxs = () => {
             <div className="box">
                 {
                     cup.map(item => (
-                        [...Array(parseInt(item.quantity)).keys()].map((e) =>{
+                        [...Array(parseInt(item.quantity)).keys()].map((e) => {
 
-                            return    <div
-                                key={item.id+e}
+                            return <div
+                                key={item.id + e}
                                 style={{
-                                    width: item.width + "px",
-                                    height: +item.height + "px",
-                                    backgroundColor: "indianred"
+                                    width: (item.width * 0.26) + "px",
+                                    height: (item.height * 0.26) + "px",
+                                    backgroundColor: item.color
                                 }}
                                 className="boxes"
                             >
--- src/form/index.js
+++ src/form/index.js
@@ -1,20 +1,29 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import './form.css'
 import {DELETE_BOX, SEND_SIZE} from "../redux/action/action";
 import Edit from "../list/list";
+import './form.css'
 
 const Form = () => {
     const [square, setSquare] = useState({width: "", height: ""})
-    const {cup,EditeBox} = useSelector(user => user.state)
+    const[colors,setColors] = useState('')
+    const {cup, EditeBox} = useSelector(user => user.state)
     const dispatch = useDispatch()
     const formHandle = (e) => {
         const {name, value} = e.target
         setSquare({...square, [name]: value})
     }
+    useEffect(()=>{
+        const val = "#" +
+            Math.floor((
+                Math.random()*(256*256*256).toString(16)
+            ))
+        setColors(val)
+    },[square])
     const sendHandle = () => {
-        dispatch({type: SEND_SIZE, payload: {...square, id: Date.now()}})
+        dispatch({type: SEND_SIZE, payload: {...square, id: Date.now(),color:colors}})
     }
+    console.log(colors,"colors")
 
     return (
         <>
@@ -33,7 +42,7 @@ const Form = () => {
                                             </tr>
                                             {
                                                 cup.map(item => (
-                                                    EditeBox.id === item.id  ?
+                                                    EditeBox.id === item.id ?
                                                         <Edit
                                                             square={square}
                                                             setSquare={setSquare}
@@ -45,16 +54,21 @@ const Form = () => {
                                                             <td>{item.width}</td>
                                                             <td>{item.height}</td>
                                                             <td>{item.quantity}</td>
-                                                            <td>56546</td>
                                                             <td>
                                                                 <button className="btnEdit"
-                                                                        onClick={() =>dispatch({type:"EDIT_BOX",payload:item})}>
+                                                                        onClick={() => dispatch({
+                                                                            type: "EDIT_BOX",
+                                                                            payload: item
+                                                                        })}>
                                                                     Edit
                                                                 </button>
                                                             </td>
                                                             <td>
                                                                 <button className="btnDelete"
-                                                                onClick={()=> dispatch({type: DELETE_BOX,payload:item.id})}
+                                                                        onClick={() => dispatch({
+                                                                            type: DELETE_BOX,
+                                                                            payload: item.id
+                                                                        })}
                                                                 >
                                                                     Delete
                                                                 </button>
@@ -73,7 +87,7 @@ const Form = () => {
                     <form>
                         <input type="number" placeholder="Width" name="width" onChange={(e) => formHandle(e)}/>
                         <input type="number" placeholder="Height" name="height" onChange={(e) => formHandle(e)}/>
-                        <input type="number" placeholder="Quantity" name="quantity"  onChange={(e) => formHandle(e)}/>
+                        <input type="number" placeholder="Quantity" name="quantity" onChange={(e) => formHandle(e)}/>
                         <input type="submit" className="btn" onClick={sendHandle} placeholder="SEND"/>
                     </form>
                 </div>
